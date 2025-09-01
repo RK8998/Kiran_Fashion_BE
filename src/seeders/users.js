37a2521fad10c08a7572872cdd4f2d781792deb1
Krushang Rathod <krushang.r@crestinfosystems.com>
2025-09-01 17:30:24 +0530

    try to fix env file and vercel json file.

--- src/seeders/users.js
+++ src/seeders/users.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '.env' });
 const mongoose = require('mongoose');
 const UserModel = require('../models/users.js');
 const { ROLES } = require('../utils/constants.js');
